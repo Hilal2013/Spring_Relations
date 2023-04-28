5c0e25d11f5c83f6d3e3cfedc2175010692502a5
Hilal2013 <hilalozkul@gmail.com>
2023-04-27 22:21:16 -0400

    Invoice

---
+++ src/main/java/com/cydeo/mentor/entity/Invoice.java
@@ -0,0 +1,35 @@
+package com.cydeo.mentor.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Invoice extends BaseEntity{
+
+    private String invoiceNumber;
+    private String invoiceType;
+
+    private LocalDate invoiceDate;
+
+/*  This field will be used in STEP-3
+    private Client client;
+ */
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceNumber='" + invoiceNumber + '\'' +
+                ", invoiceType='" + invoiceType + '\'' +
+                ", invoiceDate=" + invoiceDate +
+                '}';
+    }
+}
