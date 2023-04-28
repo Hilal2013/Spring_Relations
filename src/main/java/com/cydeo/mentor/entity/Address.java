07c55bac19a5973fc48d483fa1fd57f4bcc4acf7
Hilal2013 <hilalozkul@gmail.com>
2023-04-27 22:20:04 -0400

    Address

--- src/main/java/com/cydeo/mentor/entity/Address.java
+++ src/main/java/com/cydeo/mentor/entity/Address.java
@@ -3,15 +3,19 @@ package com.cydeo.mentor.entity;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-public class Address extends BaseEntity{
-
-   // private Long id;
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private String zipCode;
 
