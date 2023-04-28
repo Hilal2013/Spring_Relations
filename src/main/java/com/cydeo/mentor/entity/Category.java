7f2ce37e904a29bab05192d43a6cb6f0cd58454b
Hilal2013 <hilalozkul@gmail.com>
2023-04-27 22:20:18 -0400

    Category

--- src/main/java/com/cydeo/mentor/entity/Category.java
+++ src/main/java/com/cydeo/mentor/entity/Category.java
@@ -6,14 +6,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-public class Category extends BaseEntity {
-
-   // private Long id;
+public class Category  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String categoryName;
     private int tax;
 
