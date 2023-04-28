92cf30f27d24a603aada540318cc1ed13cbaa7f1
Hilal2013 <hilalozkul@gmail.com>
2023-04-27 22:19:47 -0400

    Client

---
+++ src/main/java/com/cydeo/mentor/entity/Client.java
@@ -0,0 +1,32 @@
+package com.cydeo.mentor.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client extends BaseEntity {
+
+    private String clientName;
+    private String phone;
+    private String email;
+
+/*  This field will be used in STEP-3
+    private Address address;
+ */
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientName='" + clientName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
