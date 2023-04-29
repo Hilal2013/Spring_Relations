3da4096858d4544e9c45f638d2a4d427ceeb71d5
Hilal2013 <hilalozkul@gmail.com>
2023-04-28 22:54:31 -0400

    Client

--- src/main/java/com/cydeo/mentor/entity/Client.java
+++ src/main/java/com/cydeo/mentor/entity/Client.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.List;
 
@@ -22,8 +24,8 @@ public class Client extends BaseEntity {
 /*  This field will be used in STEP-3
     private Address address;
  */
-    @OneToMany
-private List<Address> address;
+@ManyToOne
+private Address address;
     @Override
     public String toString() {
         return "Client{" +
